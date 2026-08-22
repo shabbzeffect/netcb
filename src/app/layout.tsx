356ddced48603d59ad6a847facd1e0569b2adcda
ShabbzEffect <wangarashabir@yahoo.com>
2026-08-22 21:56:06 +0300

    Fix favicon: use NETCB logo from netcb.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,10 +79,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon-32.png" sizes="32x32" type="image/png" />
-        <link rel="icon" href="/favicon-16.png" sizes="16x16" type="image/png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/favicon.png" sizes="180x180" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
